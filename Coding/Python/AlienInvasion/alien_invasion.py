7e5906127425da7b118555f29ca65101fea1640b
SpikeBeboop <spikebeboop@163.com>
2025-06-03 09:50:17 +0800

    飞机大战，武装飞机

---
+++ Coding/Python/AlienInvasion/alien_invasion.py
@@ -0,0 +1,61 @@
+import sys
+
+import pygame
+
+from src.settings import Settings
+from src.ship import Ship
+
+
+class AlienInvasion:
+    # 管理游戏资源和行为的类
+    def __init__(self):
+        # 初始化游戏并创建游戏资源
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+
+    def _check_events(self):
+        # 监视键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    #检查按键按下事件
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_d:
+            self.ship.moving_right = True
+        if event.key == pygame.K_a:
+            self.ship.moving_left = True
+        if event.key == pygame.K_q:
+            sys.exit()
+
+    #检查按键弹起事件
+    def _check_keyup_events(self,event):
+        if event.key == pygame.K_d:
+            self.ship.moving_right = False
+        if event.key == pygame.K_a:
+            self.ship.moving_left = False
+
+    #更新屏幕
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+
+    def run_game(self):
+        # 开始游戏的主循环
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
